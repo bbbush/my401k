24116b0e0476733c835dc2260263f0760404ec0b
Yuan Yijun <bbbush.yuan@gmail.com>
2013-03-10 19:38:38 -0500

    updated salary after 6 payments

--- my401k.py
+++ my401k.py
@@ -1,9 +1,11 @@
 import ccontribution
 
 if __name__=="__main__":
-    salary=70000
-    exclusion=salary/10
+    nm=24
+    rnm=18
+    salary=72000.0 # updated
+    exclusion=10000 # data from ADP
     limit=17500-exclusion # exclude expected bonus contribution and all contributions so far
-    c=ccontribution.ccontribution(limit,salary,24)
+    c=ccontribution.ccontribution(limit,salary*rnm/nm,rnm)
     c.all_reports()
 
